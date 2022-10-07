2874a2fcc7d21404d66678aeea88389e7fb9fdde
William Sampaio <1997wbms@gmail.com>
2022-10-07 01:18:01 -0400

    add scripts

---
+++ install.sh
@@ -0,0 +1,32 @@
+# Uninstall old versions
+apt remove docker docker-engine docker.io containerd runc -y
+
+#Set up the repository
+apt update
+apt install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+apt install docker-compose -y
+
+# Manage Docker as a non-root user
+groupadd docker
+usermod -aG docker $USER
+newgrp docker
+
+#Configure Docker to start on boot
+systemctl enable docker.service
+systemctl enable containerd.service
+
+echo 'Docker and Docker-Compose Installed!'
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+apt purge docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+rm -rf /var/lib/docker
+rm -rf /var/lib/containerd
