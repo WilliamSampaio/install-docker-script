4319ef10592b9e4cc8c7b5d2625b377dba04a6a4
William Sampaio <1997wbms@gmail.com>
2022-10-07 01:29:57 -0400

    update

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 apt purge docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 apt purge docker-compose -y
-apt autoremove
+apt autoremove -y
 rm -rf /var/lib/docker
 rm -rf /var/lib/containerd
